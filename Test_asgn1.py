c9d89dd811e014a68ce06b2d73034d7bda6d9b86
hyfung <43932013+hyfung@users.noreply.github.com>
2020-11-30 09:56:43 +0800

    Create Test_asgn1.py

---
+++ Test_asgn1.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import unittest
+
+class TestMain(unittest.TestCase):
+    def test_compute_sales():
+        pass
+
+    def test_main():
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
